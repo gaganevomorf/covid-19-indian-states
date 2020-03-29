f7c8502e138bc13ad2b2ec036421feea3e4438d5
gaganevomorf2019 <gagan@evomorf.com>
2020-03-30 01:59:22 +0530

    Updates

---
+++ precache-manifest.0a3cfa837d01fa1d5c720a10dc908ecf.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09cfadc673ff337dc2012c3347c7a9a",
+    "url": "/covid-19-indian-states/index.html"
+  },
+  {
+    "revision": "3589d9cba53f0dbe1acd",
+    "url": "/covid-19-indian-states/static/css/51.f6437998.chunk.css"
+  },
+  {
+    "revision": "6d6b56c6160d0cc26045",
+    "url": "/covid-19-indian-states/static/css/main.c2053e2f.chunk.css"
+  },
+  {
+    "revision": "3231a0ac2265f29ce4b7",
+    "url": "/covid-19-indian-states/static/js/0.5fce0c23.chunk.js"
+  },
+  {
+    "revision": "1537b1fedb0149a681c8",
+    "url": "/covid-19-indian-states/static/js/1.dd49d39d.chunk.js"
+  },
+  {
+    "revision": "0f64558d80acf9cd982a",
+    "url": "/covid-19-indian-states/static/js/2.08b8b5bd.chunk.js"
+  },
+  {
+    "revision": "9c7c8b689880a70f8586",
+    "url": "/covid-19-indian-states/static/js/3.7c50bcf9.chunk.js"
+  },
+  {
+    "revision": "5f238995f1127bcab44c",
+    "url": "/covid-19-indian-states/static/js/4.924ad24f.chunk.js"
+  },
+  {
+    "revision": "cfba46fe6c98d1767042",
+    "url": "/covid-19-indian-states/static/js/5.6bec05f7.chunk.js"
+  },
+  {
+    "revision": "3589d9cba53f0dbe1acd",
+    "url": "/covid-19-indian-states/static/js/51.5623a100.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid-19-indian-states/static/js/51.5623a100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82642c01b7ffd12758da",
+    "url": "/covid-19-indian-states/static/js/52.9ab4bdc5.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/covid-19-indian-states/static/js/52.9ab4bdc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556944a03d3fd87e3e6d",
+    "url": "/covid-19-indian-states/static/js/53.e2d61f90.chunk.js"
+  },
+  {
+    "revision": "1b57cf7f4194618fa251",
+    "url": "/covid-19-indian-states/static/js/54.0ac326fe.chunk.js"
+  },
+  {
+    "revision": "9ab470b270d7b5f59600",
+    "url": "/covid-19-indian-states/static/js/55.b8a331b6.chunk.js"
+  },
+  {
+    "revision": "cd8b3c5f36b78997c219",
+    "url": "/covid-19-indian-states/static/js/56.78e34718.chunk.js"
+  },
+  {
+    "revision": "d5a631ba25613b365b24",
+    "url": "/covid-19-indian-states/static/js/57.c5ad3bb4.chunk.js"
+  },
+  {
+    "revision": "a114ace8cc087505c7be",
+    "url": "/covid-19-indian-states/static/js/58.0da42fb9.chunk.js"
+  },
+  {
+    "revision": "b2b77cc4cb5187b8919a",
+    "url": "/covid-19-indian-states/static/js/59.93785c18.chunk.js"
+  },
+  {
+    "revision": "30a5aebaa5e87b7e3ab0",
+    "url": "/covid-19-indian-states/static/js/60.fc154873.chunk.js"
+  },
+  {
+    "revision": "e654bd1d57f37d245dcc",
+    "url": "/covid-19-indian-states/static/js/61.471b47dc.chunk.js"
+  },
+  {
+    "revision": "97449826cb4fc26abdb2",
+    "url": "/covid-19-indian-states/static/js/62.2349771a.chunk.js"
+  },
+  {
+    "revision": "d1aa6b9b8a8f95a4f130",
+    "url": "/covid-19-indian-states/static/js/63.f000d666.chunk.js"
+  },
+  {
+    "revision": "6b7507914f431890ca26",
+    "url": "/covid-19-indian-states/static/js/64.6efa0926.chunk.js"
+  },
+  {
+    "revision": "f9689205b490e1cd48c0",
+    "url": "/covid-19-indian-states/static/js/65.530a6247.chunk.js"
+  },
+  {
+    "revision": "d3c65b33c3684ad11a1e",
+    "url": "/covid-19-indian-states/static/js/66.f1a7a341.chunk.js"
+  },
+  {
+    "revision": "83189ac3745834760b00",
+    "url": "/covid-19-indian-states/static/js/67.faae8f0d.chunk.js"
+  },
+  {
+    "revision": "c7d5628f4a4f61976536",
+    "url": "/covid-19-indian-states/static/js/68.83ed45dd.chunk.js"
+  },
+  {
+    "revision": "94ca4ca911919dec8fcb",
+    "url": "/covid-19-indian-states/static/js/69.ada61f15.chunk.js"
+  },
+  {
+    "revision": "022cf01e5a394242e946",
+    "url": "/covid-19-indian-states/static/js/70.38916cde.chunk.js"
+  },
+  {
+    "revision": "782f80c40611e2189308",
+    "url": "/covid-19-indian-states/static/js/71.6ea16722.chunk.js"
+  },
+  {
+    "revision": "f3b35630c84d232d8e7c",
+    "url": "/covid-19-indian-states/static/js/72.4d0272d5.chunk.js"
+  },
+  {
+    "revision": "e135c380530faafe1759",
+    "url": "/covid-19-indian-states/static/js/73.459a2769.chunk.js"
+  },
+  {
+    "revision": "2324184bfa684227ab4b",
+    "url": "/covid-19-indian-states/static/js/74.d3b56ff0.chunk.js"
+  },
+  {
+    "revision": "15a0a9edf963eebbaa7e",
+    "url": "/covid-19-indian-states/static/js/75.fcbae445.chunk.js"
+  },
+  {
+    "revision": "c927f62d84f9e5cd6146",
+    "url": "/covid-19-indian-states/static/js/76.70487207.chunk.js"
+  },
+  {
+    "revision": "0a430fa5a672a217d6f5",
+    "url": "/covid-19-indian-states/static/js/77.d51ed680.chunk.js"
+  },
+  {
+    "revision": "e7abfda5b067dea50c14",
+    "url": "/covid-19-indian-states/static/js/78.5d981b7f.chunk.js"
+  },
+  {
+    "revision": "6f9c52150434479f2e3f",
+    "url": "/covid-19-indian-states/static/js/79.d62883ba.chunk.js"
+  },
+  {
+    "revision": "8aff1d5a3cfd20fd1921",
+    "url": "/covid-19-indian-states/static/js/80.69f5beda.chunk.js"
+  },
+  {
+    "revision": "4707b57926ebb16eb6a7",
+    "url": "/covid-19-indian-states/static/js/81.f9bd8252.chunk.js"
+  },
+  {
+    "revision": "4f2f71aeb3f87dd7fbe5",
+    "url": "/covid-19-indian-states/static/js/82.6b93c94d.chunk.js"
+  },
+  {
+    "revision": "16385441cc446a51a473",
+    "url": "/covid-19-indian-states/static/js/83.0e4622ce.chunk.js"
+  },
+  {
+    "revision": "ba2e6ba50cbb183d061c",
+    "url": "/covid-19-indian-states/static/js/84.f4ef255d.chunk.js"
+  },
+  {
+    "revision": "82ffb7cb1fe2a52413d3",
+    "url": "/covid-19-indian-states/static/js/85.4234cc60.chunk.js"
+  },
+  {
+    "revision": "7d9f71350f1f4cccfcbf",
+    "url": "/covid-19-indian-states/static/js/86.01705b9e.chunk.js"
+  },
+  {
+    "revision": "857a1f7f347dbf421cda",
+    "url": "/covid-19-indian-states/static/js/87.6368b8d5.chunk.js"
+  },
+  {
+    "revision": "61d19ab731d10911b4ed",
+    "url": "/covid-19-indian-states/static/js/88.644e332a.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/covid-19-indian-states/static/js/88.644e332a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962cb280439e84f7da2b",
+    "url": "/covid-19-indian-states/static/js/89.cbf04ccf.chunk.js"
+  },
+  {
+    "revision": "09e82b9cc0ce56494694",
+    "url": "/covid-19-indian-states/static/js/90.f6caca3b.chunk.js"
+  },
+  {
+    "revision": "2f4619a221da1012ede3",
+    "url": "/covid-19-indian-states/static/js/91.ff5dd725.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/covid-19-indian-states/static/js/91.ff5dd725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6b56c6160d0cc26045",
+    "url": "/covid-19-indian-states/static/js/main.0c2aa858.chunk.js"
+  },
+  {
+    "revision": "3bdfef37bdf33992d826",
+    "url": "/covid-19-indian-states/static/js/runtime-main.7cd84983.js"
+  },
+  {
+    "revision": "88080b2f18dad24290a2",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-avatar_3-ios-entry-js.92ba2d8d.chunk.js"
+  },
+  {
+    "revision": "a8b45d7c3b6d8cfbf82e",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-avatar_3-md-entry-js.f6cd43d8.chunk.js"
+  },
+  {
+    "revision": "b642f3d9c04700eb5537",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-back-button-ios-entry-js.14c17d44.chunk.js"
+  },
+  {
+    "revision": "c8f71c9f59ef9e17f6d2",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-back-button-md-entry-js.09855925.chunk.js"
+  },
+  {
+    "revision": "e9f9c29e64ce8dc27933",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-backdrop-ios-entry-js.327ae51a.chunk.js"
+  },
+  {
+    "revision": "ed7f8fd68ad5aee75927",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-backdrop-md-entry-js.bcc17179.chunk.js"
+  },
+  {
+    "revision": "53a66aa2ff56f8cddc34",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-card_5-ios-entry-js.0a57d01d.chunk.js"
+  },
+  {
+    "revision": "2e89b4ac7c6390143644",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-card_5-md-entry-js.1a4822ed.chunk.js"
+  },
+  {
+    "revision": "d9e715438617612d2f3d",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-checkbox-ios-entry-js.62862e47.chunk.js"
+  },
+  {
+    "revision": "e063da22c0a682f25d88",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-checkbox-md-entry-js.01769aa4.chunk.js"
+  },
+  {
+    "revision": "740b484e717c9a892447",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-chip-ios-entry-js.d047b92a.chunk.js"
+  },
+  {
+    "revision": "d1c104714a15b187f432",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-chip-md-entry-js.48892fc2.chunk.js"
+  },
+  {
+    "revision": "5da8068d87903e8a3ef3",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-col_3-entry-js.10354769.chunk.js"
+  },
+  {
+    "revision": "200fb30627ed15cba95e",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-fab_3-ios-entry-js.915882c9.chunk.js"
+  },
+  {
+    "revision": "635dcdad48cc3eab8973",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-fab_3-md-entry-js.c4e3ce01.chunk.js"
+  },
+  {
+    "revision": "eb119e50e4b35b38866f",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-img-entry-js.f323ba50.chunk.js"
+  },
+  {
+    "revision": "379d3e8fe7fe4ac62f55",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-infinite-scroll_2-ios-entry-js.7765ab73.chunk.js"
+  },
+  {
+    "revision": "3bda7e9d1baefa7357e7",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-infinite-scroll_2-md-entry-js.c1900f9d.chunk.js"
+  },
+  {
+    "revision": "ab9bd98a9fccaec172fa",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-input-ios-entry-js.03e70114.chunk.js"
+  },
+  {
+    "revision": "016074cf0d213d0e6d17",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-input-md-entry-js.97567014.chunk.js"
+  },
+  {
+    "revision": "cc2e0c94d20134fcf6e7",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-loading-ios-entry-js.42dfa012.chunk.js"
+  },
+  {
+    "revision": "fdbc7cc8ace3dc2c2299",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-loading-md-entry-js.f2293a02.chunk.js"
+  },
+  {
+    "revision": "9ff305fe132a3efb116d",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-popover-ios-entry-js.ea559c6f.chunk.js"
+  },
+  {
+    "revision": "15a35b2401deeca05de6",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-popover-md-entry-js.fb816a35.chunk.js"
+  },
+  {
+    "revision": "cb71cb2e86f362fe0e5e",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-progress-bar-ios-entry-js.809c3b8f.chunk.js"
+  },
+  {
+    "revision": "6576dfdc8aa4a55d653d",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-progress-bar-md-entry-js.bc7cf435.chunk.js"
+  },
+  {
+    "revision": "dd0c220ec6044bba6692",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-radio_2-ios-entry-js.636b1fe2.chunk.js"
+  },
+  {
+    "revision": "333b7bcb44054504d7dc",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-radio_2-md-entry-js.4a94b2e2.chunk.js"
+  },
+  {
+    "revision": "be3fae17c6e4ed788345",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-reorder_2-ios-entry-js.80b1ca40.chunk.js"
+  },
+  {
+    "revision": "42437d60c0ebbf2c0eac",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-reorder_2-md-entry-js.4d721834.chunk.js"
+  },
+  {
+    "revision": "e7363e8876b62f578b2f",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-ripple-effect-entry-js.00f66749.chunk.js"
+  },
+  {
+    "revision": "3f6dcd260bf2346b0a6b",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-spinner-entry-js.11ec97f3.chunk.js"
+  },
+  {
+    "revision": "13e549554fc70e8bab37",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-split-pane-ios-entry-js.67fd1f94.chunk.js"
+  },
+  {
+    "revision": "3ebbb30f679ed5538e53",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-split-pane-md-entry-js.bf8c0965.chunk.js"
+  },
+  {
+    "revision": "9e5a3e0716ce7a4202f2",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-tab-bar_2-ios-entry-js.0b5b9d33.chunk.js"
+  },
+  {
+    "revision": "09b9b877a171d6beb5d1",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-tab-bar_2-md-entry-js.257baada.chunk.js"
+  },
+  {
+    "revision": "15559c0b8a0a51347b8e",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-tab_2-entry-js.fd93c667.chunk.js"
+  },
+  {
+    "revision": "4e0c56f0a5a183324ee4",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-text-entry-js.e8b8afd3.chunk.js"
+  },
+  {
+    "revision": "64f914453844337ad9cf",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-textarea-ios-entry-js.01a08bf8.chunk.js"
+  },
+  {
+    "revision": "5298231ccf16efdde19a",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-textarea-md-entry-js.922f12c6.chunk.js"
+  },
+  {
+    "revision": "e7e45bb1be205d6e3fc1",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-toggle-ios-entry-js.e673a775.chunk.js"
+  },
+  {
+    "revision": "9775d0b2c173d165dcfd",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-toggle-md-entry-js.a5b9072c.chunk.js"
+  },
+  {
+    "revision": "3ab1163284f0b5fcd02b",
+    "url": "/covid-19-indian-states/static/js/stencil-ion-virtual-scroll-entry-js.bb1c18bb.chunk.js"
+  }
+]);
